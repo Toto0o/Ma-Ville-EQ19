6373c72fcde8da3eebd92f83a1ae2de5e99e250a
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2024-11-12 23:42:05 -0500

    Initial commit for firebase (still not functional)

--- Prototype/GUI.java
+++ Prototype/GUI.java
@@ -1,6 +1,7 @@
 package Prototype;
 
 import Prototype.Controllers.SceneController;
+import Prototype.firebase.FirebaseConfig;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -8,11 +9,12 @@ public class GUI extends Application {
 
     private SceneController sceneController;
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         GUI.launch(args);
     }
-    
+
     public void start(Stage primaryStage) throws Exception {
+        FirebaseConfig.initializeFirebase();
         this.sceneController = new SceneController(primaryStage);
         this.sceneController.start();
     }
--- Prototype/Scenes/RegisterScene.java
+++ Prototype/Scenes/RegisterScene.java
@@ -8,9 +8,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
-public class RegisterScene extends Scenes{
+public class RegisterScene extends Scenes {
 
-    private TextField nameField, lastNameField, birthdayField, addressField, emailField, phoneField, password1Field, password2Field;
+    private TextField nameField, lastNameField, birthdayField, addressField, emailField, phoneField, password1Field,
+            password2Field;
     private Text nameText, lastNameText, birthdayText, addressText, emailText, phoneText, password1Text, password2Text;
 
     private Button registerButton, menu;
@@ -50,7 +51,7 @@ public class RegisterScene extends Scenes{
         /* this.credentialsVerifier = new CredentialsVerifier(); */
 
         this.errorAddress = new Text("Address is invalid, please enter a valid address");
-        this.errorPassword = new Text("Passwords are not matching... please enter matching passwords"); 
+        this.errorPassword = new Text("Passwords are not matching... please enter matching passwords");
 
     }
 
@@ -59,54 +60,51 @@ public class RegisterScene extends Scenes{
         this.root.setCenter(this.vBox);
         this.vBox.setAlignment(Pos.CENTER);
         this.vBox.getChildren().addAll(
-            this.nameText,
-            this.nameField,
-            this.lastNameText,
-            this.lastNameField,
-            this.birthdayText,
-            this.birthdayField,
-            this.addressText,
-            this.addressField,
-            this.emailText,
-            this.emailField,
-            this.phoneText,
-            this.phoneField,
-            this.password1Text,
-            this.password1Field,
-            this.password2Text,
-            this.password2Field,
-            this.registerButton
-            );
-            this.nameField.setMaxWidth(250);
-            this.lastNameField.setMaxWidth(250);
-            this.birthdayField.setMaxWidth(250);
-            this.addressField.setMaxWidth(250);
-            this.emailField.setMaxWidth(250);
-            this.phoneField.setMaxWidth(250);
-            this.password1Field.setMaxWidth(250);
-            this.password2Field.setMaxWidth(250);
+                this.nameText,
+                this.nameField,
+                this.lastNameText,
+                this.lastNameField,
+                this.birthdayText,
+                this.birthdayField,
+                this.addressText,
+                this.addressField,
+                this.emailText,
+                this.emailField,
+                this.phoneText,
+                this.phoneField,
+                this.password1Text,
+                this.password1Field,
+                this.password2Text,
+                this.password2Field,
+                this.registerButton);
+        this.nameField.setMaxWidth(250);
+        this.lastNameField.setMaxWidth(250);
+        this.birthdayField.setMaxWidth(250);
+        this.addressField.setMaxWidth(250);
+        this.emailField.setMaxWidth(250);
+        this.phoneField.setMaxWidth(250);
+        this.password1Field.setMaxWidth(250);
+        this.password2Field.setMaxWidth(250);
 
-
-            registerButton.setOnMouseClicked((registerAction) -> {
-                if (this.credentialsVerifier.verifyAdress(this.addressField.getText())) {
-                    if (this.credentialsVerifier.verifiyMatchingPasswords(this.password1Field.getText(), this.password2Field.getText())) {
-                        this.sceneController.newScene("menu");
-                    }
-                    else {
-                        this.vBox.getChildren().add(this.errorPassword);
-                        this.password1Field.clear();
-                        this.password2Field.clear();
-                    }
+        registerButton.setOnMouseClicked((registerAction) -> {
+            if (this.credentialsVerifier.verifyAdress(this.addressField.getText())) {
+                if (this.credentialsVerifier.verifiyMatchingPasswords(this.password1Field.getText(),
+                        this.password2Field.getText())) {
+                    this.sceneController.newScene("menu");
+                } else {
+                    this.vBox.getChildren().add(this.errorPassword);
+                    this.password1Field.clear();
+                    this.password2Field.clear();
                 }
-                else {
-                    this.vBox.getChildren().add(this.errorAddress);
-                    this.addressField.clear();
-                }
-            });
+            } else {
+                this.vBox.getChildren().add(this.errorAddress);
+                this.addressField.clear();
+            }
+        });
 
-            this.menu.setOnMouseClicked((retourAction) -> {
-                this.sceneController.newScene("launch");
-            });
+        this.menu.setOnMouseClicked((retourAction) -> {
+            this.sceneController.newScene("launch");
+        });
 
     }
 
